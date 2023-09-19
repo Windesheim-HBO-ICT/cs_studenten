41b3e284869d22b43bccc122194d09254e43a3ba
JBrouwers-Windesheim <j.c.t.brouwers@windesheim.nl>
2023-09-19 18:10:49 +0200

    huiswerkopdrachten week 4

---
+++ lessen/week-4/les-2/huiswerkopdrachten/Opdracht_Studiefinanciering/OverheidLibrary/Studiefinanciering.cs
@@ -0,0 +1,26 @@
+﻿namespace OverheidLibrary
+{
+    public delegate void UitbetalingDelegate(decimal bedrag);
+
+    public class Studiefinanciering
+    {
+        public decimal LandelijkBudget { get; set; }
+
+        public Studiefinanciering(decimal landelijkBudget)
+        {
+            LandelijkBudget = landelijkBudget;
+        }
+
+        public void VerrichtBetalingen()
+        {
+            if (UitbetalingEvent != null)
+            {
+                var aantalStudenten = UitbetalingEvent.GetInvocationList().Length;
+                var studentToelage = LandelijkBudget / aantalStudenten;
+                UitbetalingEvent(studentToelage);
+            }
+        }
+
+        public UitbetalingDelegate? UitbetalingEvent;
+    }
+}
