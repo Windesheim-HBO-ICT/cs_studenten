cd16f3fe206965927e9f1b9655d37f359ca9c51d
JBrouwers-Windesheim <j.c.t.brouwers@windesheim.nl>
2023-09-27 17:32:02 +0200

    Uitwerking StringCalculator en huiswerkopdracht

---
+++ lessen/week-3/les-1 Exception handling en Unit testing/Uitwerking_StringCalculator/StringCalculatorApp/Program.cs
@@ -0,0 +1,6 @@
+﻿
+
+using StringCalculatorApp;
+
+StringCalculator stringCalculator = new StringCalculator();
+stringCalculator.Add("1,2,3");
---
+++ lessen/week-3/les-1 Exception handling en Unit testing/Uitwerking_StringCalculator/StringCalculatorApp/StringCalculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculatorApp
+{
+    public class StringCalculator
+    {
+        public int Add(string input) 
+        {
+            string[] inputAsArray = input.Split(',');
+            int result = 0;
+            for (int i = 0; i < inputAsArray.Length; i++)
+            {
+                int number;
+                if (int.TryParse(inputAsArray[i], out number))
+                {
+                    result += number;
+                }
+                else 
+                {
+                    throw new FormatException($"Input {inputAsArray[i]} given to {nameof(Add)} cannot be calculated.");
+                }
+            }
+            return result;
+        }
+    }
+}
