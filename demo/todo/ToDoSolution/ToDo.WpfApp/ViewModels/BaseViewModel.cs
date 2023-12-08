e9919312db0fb3ced11e4cf32ed0d563146c9273
JBrouwers-Windesheim <j.c.t.brouwers@windesheim.nl>
2023-12-08 15:39:52 +0100

    ToDo Application C4-models

---
+++ demo/todo/ToDoSolution/ToDo.WpfApp/ViewModels/BaseViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDo.WpfApp.ViewModels
+{
+    public abstract class BaseViewModel
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void PropertyChanging([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
