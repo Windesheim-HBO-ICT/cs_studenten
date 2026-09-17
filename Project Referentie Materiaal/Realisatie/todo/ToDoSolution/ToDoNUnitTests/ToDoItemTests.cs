28b06ecda31572fed6072bda041b234cfb5c08bd
JBrouwers-Windesheim <j.c.t.brouwers@windesheim.nl>
2026-09-17 09:09:58 +0200

    Testproject migrated from xUnit to NUnit

---
+++ Project Referentie Materiaal/Realisatie/todo/ToDoSolution/ToDoNUnitTests/ToDoItemTests.cs
@@ -0,0 +1,24 @@
+﻿using ToDo;
+
+
+namespace ToDoNUnitTests
+{
+    public class ToDoItemTests
+    {
+        [Test]
+        public void EmptyTitle_ThrowsException()
+        {
+            var toDoItem = new ToDoItem("")
+            {
+                AssignedTo = "someone",
+                Description = "something",
+                Due = DateTime.Now.AddDays(1),
+                Id = 1,
+                State = ToDoState.Undetermined
+            };
+            var todoService = new ToDoService(null);
+
+            Assert.Throws<InvalidOperationException>(() => todoService.ValidateToDoItem(toDoItem));
+        }
+    }
+}
